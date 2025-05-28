298c4ef6fb41b25be074c5d3412003d29112fe94
Creastv <creastv@gail.com>
2025-05-28 14:55:33 +0200

    first push

--- blocks/posts-grid/post-grid-last.php
+++ blocks/posts-grid/post-grid-last.php
@@ -25,27 +25,27 @@ $query_posts_two = new WP_Query($po2);
                 $query_posts_one->the_post();
                 $term_list = wp_get_post_terms(get_the_id(), 'category');
             ?>
-                <article class="post">
-                    <div class="post__img">
+            <article class="post">
+                <div class="post__img">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('large'); ?>
+                    </a>
+                </div>
+                <div class="post__content">
+                    <div class="entry-term">
+                        <?php foreach ($term_list as $term): ?>
+                        <a
+                            href="<?php echo get_term_link($term->term_id, 'category'); ?>"><?php echo $term->name; ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                    <h2 class="entry-title">
                         <a href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail('large'); ?>
+                            <?php the_title(); ?>
                         </a>
-                    </div>
-                    <div class="post__content">
-                        <div class="entry-term">
-                            <?php foreach ($term_list as $term): ?>
-                                <a
-                                    href="<?php echo get_term_link($term->term_id, 'category'); ?>"><?php echo $term->name; ?></a>
-                            <?php endforeach; ?>
-                        </div>
-                        <h2 class="entry-title">
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </h2>
-                        <p class="entry-content"><?php echo custom_excerpt(40); ?></p>
-                    </div>
-                </article>
+                    </h2>
+                    <p class="entry-content"><?php echo custom_excerpt(40); ?></p>
+                </div>
+            </article>
             <?php }
             wp_reset_postdata(); ?>
         </div>
@@ -54,27 +54,27 @@ $query_posts_two = new WP_Query($po2);
                 $query_posts_two->the_post();
                 $term_list = wp_get_post_terms(get_the_id(), 'category');
             ?>
-                <article class="post-vertical">
-                    <div class="post__img">
+            <article class="post-vertical">
+                <div class="post__img">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('medium'); ?>
+                    </a>
+                </div>
+                <div class="post__content">
+                    <div class="entry-term">
+                        <?php foreach ($term_list as $term): ?>
+                        <a
+                            href="<?php echo get_term_link($term->term_id, 'category'); ?>"><?php echo $term->name; ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                    <h2 class="entry-title">
                         <a href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail('thumbnail'); ?>
+                            <?php the_title(); ?>
                         </a>
-                    </div>
-                    <div class="post__content">
-                        <div class="entry-term">
-                            <?php foreach ($term_list as $term): ?>
-                                <a
-                                    href="<?php echo get_term_link($term->term_id, 'category'); ?>"><?php echo $term->name; ?></a>
-                            <?php endforeach; ?>
-                        </div>
-                        <h2 class="entry-title">
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </h2>
-                        <p class="entry-content"><?php echo custom_excerpt(10); ?></p>
-                    </div>
-                </article>
+                    </h2>
+                    <p class="entry-content"><?php echo custom_excerpt(10); ?></p>
+                </div>
+            </article>
             <?php }
             wp_reset_postdata(); ?>
         </div>
